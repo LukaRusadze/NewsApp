17f00fb7248b7aca2332137d68c999cebf85bba4
LukaRusadze <rusadzeluka@gmail.com>
2023-04-03 10:49:54 +0400

    feat(android): make statusbar translucent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {ThemeProvider} from '@rneui/themed';
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import MainStack from './navigation';
 import {store} from './store';
@@ -7,6 +8,11 @@ import {store} from './store';
 const App = () => {
   return (
     <ThemeProvider>
+      <StatusBar
+        translucent
+        backgroundColor={'transparent'}
+        barStyle={'dark-content'}
+      />
       <Provider store={store}>
         <MainStack />
       </Provider>
